07920ecd8a67e38152992eb754ed33e47a8da50f
Akashamba <akashambashankar@gmail.com>
2021-01-11 18:34:11 +0530

    added pwa template

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
@@ -11,7 +12,7 @@ function App() {
         </p>
         <a
           className="App-link"
-          href="https://github.com/Akashamba/todo-list-app"
+          href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
