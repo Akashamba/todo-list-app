4d8444f60efff38740c91a0a532347e570c80788
Akashamba <akashambashankar@gmail.com>
2021-01-12 15:15:56 +0530

    added swipeable routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,8 +2,10 @@ import React from 'react';
 import {Container} from '@material-ui/core';
 import AddTask from './pages/add-task/add-task.component';
 import ViewTasks from './pages/view-tasks/view-tasks.component.jsx';
+import SettingsPage from './pages/settings-page/settings-page.component.jsx';
 import Navbar from './components/navbar/navbar.component.jsx';
 import {Switch, Route, Redirect} from 'react-router-dom';
+import SwipeableRoutes from "react-swipeable-routes"
 
 function App() {
 
@@ -11,13 +13,14 @@ function App() {
     <div className="App">
         <Navbar/>
         <Container>
-            <Switch>
+            <SwipeableRoutes>
                 <Route exact path='/'>
                     <Redirect to='/view-tasks'/>
                 </Route>
                 <Route exact path='/add-task' component={AddTask} />
                 <Route exact path='/view-tasks' component={ViewTasks} />
-            </Switch>
+                <Route exact path='/settings' component={SettingsPage} />
+            </SwipeableRoutes>
         </Container>
     </div>
   );
